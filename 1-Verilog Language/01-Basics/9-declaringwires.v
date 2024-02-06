283de5aa38130acaad346481b0f0ae444554bf05
Raveem KG <gkraveem@gmail.com>
2024-02-06 15:05:27 +0530

    Adding Basics Solutions

---
+++ 1-Verilog Language/01-Basics/9-declaringwires.v
@@ -0,0 +1,15 @@
+`default_nettype none
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+	wire and1, and2, or1;
+    assign and1 = a & b;
+    assign and2 = c & d;
+    assign or1 = and1 | and2;
+    assign out = or1;
+    assign out_n = ~or1;
+endmodule
