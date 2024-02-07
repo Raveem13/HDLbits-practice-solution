e1f3a63213dcde6a3ec9555ee725104ae3664f21
Raveem KG <gkraveem@gmail.com>
2024-02-07 14:07:20 +0530

    Multiplexers solutions added

--- 2-Circuits/07-Multiplexers/065-256to1-4bitmux.v
+++ 2-Circuits/07-Multiplexers/065-256to1-4bitmux.v
@@ -13,32 +13,31 @@ module top_module(
 	end
 endmodule
 
-//  Using generate block 
-/*
-    module top_module( 
-        input [1023:0] in,
-        input [7:0] sel,
-        output reg [3:0] out
-    );
+        //  Using generate block 
+        /*
+        module top_module( 
+            input [1023:0] in,
+            input [7:0] sel,
+            output reg [3:0] out
+        );
 
-    reg [11:0] i; // Define i as a reg
+            reg [11:0] i; // Define i as a reg
 
-    always @(*) begin
-        i = sel * 4; // Calculate starting index for slicing
-    end
-
-    // Use a generate block to conditionally assign values to out
-    genvar j;
-    generate
-        for (j = 0; j < 4; j = j + 1) begin : gen_slice
             always @(*) begin
-                if (j < 4) begin
-                    out[j] = in[i + j]; // Perform slicing operation
-                end
+                i = sel * 4; // Calculate starting index for slicing
             end
-        end
-    endgenerate
 
-    endmodule
+            // Use a generate block to conditionally assign values to out
+            genvar j;
+            generate
+                for (j = 0; j < 4; j = j + 1) begin : gen_slice
+                    always @(*) begin
+                        if (j < 4) begin
+                            out[j] = in[i + j]; // Perform slicing operation
+                        end
+                    end
+                end
+            endgenerate
 
-*/
+        endmodule
+        */
