f54885aacd62cd21f66424c0c04935db4e85a4c3
Raveem KG <gkraveem@gmail.com>
2024-02-09 14:24:59 +0530

    Latches & Flip-flops solved

--- 3-Circuits-Sequential Logic/10-Latches and Flip-Flops/097-Edge-capture-detection.v
+++ 3-Circuits-Sequential Logic/10-Latches and Flip-Flops/097-Edge-capture-detection.v
@@ -17,39 +17,39 @@ endmodule
 
 // Solution based on below truth table I built
 /*
-| nedge | reset | Qn | Qn+1 |
-|-------|-------|----|------|
-|   0   |   0   |  0 |   0  |
-|   0   |   0   |  1 |   1  |
-|   0   |   1   |  0 |   0  |
-|   0   |   1   |  1 |   0  |
-|   1   |   0   |  0 |   1  |
-|   1   |   0   |  1 |   1  |
-|   1   |   1   |  0 |   0  |
-|   1   |   1   |  1 |   0  |
+    | nedge | reset | Qn | Qn+1 |
+    |-------|-------|----|------|
+    |   0   |   0   |  0 |   0  |
+    |   0   |   0   |  1 |   1  |
+    |   0   |   1   |  0 |   0  |
+    |   0   |   1   |  1 |   0  |
+    |   1   |   0   |  0 |   1  |
+    |   1   |   0   |  1 |   1  |
+    |   1   |   1   |  0 |   0  |
+    |   1   |   1   |  1 |   0  |
 */
 
 
 //Below is Non-working solution Debug done during solving
 /*
 
-module top_module (
-    input clk,
-    input reset,
-    input [31:0] in,
-    output [31:0] out
-);
-    wire [31:0] preout , nedge;
-    wire prerst;
-    wire tout;
-    always @(posedge clk) begin
-        preout <= in;
-        //prerst <= reset;
-        nedge[1] <= ~in[1] & preout[1];
-        out[1] <= (~in[1] & preout[1]) | (~reset & out[1]);
-        //out[4] <= (~in[4] & preout[4]) | (~{32{reset}} & out[4]);
-        //out[1] <= tout;
-    end
-endmodule
+    module top_module (
+        input clk,
+        input reset,
+        input [31:0] in,
+        output [31:0] out
+    );
+        wire [31:0] preout , nedge;
+        wire prerst;
+        wire tout;
+        always @(posedge clk) begin
+            preout <= in;
+            //prerst <= reset;
+            nedge[1] <= ~in[1] & preout[1];
+            out[1] <= (~in[1] & preout[1]) | (~reset & out[1]);
+            //out[4] <= (~in[4] & preout[4]) | (~{32{reset}} & out[4]);
+            //out[1] <= tout;
+        end
+    endmodule
 
 */
