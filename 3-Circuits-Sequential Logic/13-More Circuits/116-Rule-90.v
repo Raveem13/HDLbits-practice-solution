52e80a8ace60352b99dbdaf805813927a72e0716
Raveem KG <gkraveem@gmail.com>
2024-02-10 17:00:47 +0530

    More circuits solutions added

--- 3-Circuits-Sequential Logic/13-More Circuits/116-Rule-90.v
+++ 3-Circuits-Sequential Logic/13-More Circuits/116-Rule-90.v
@@ -17,27 +17,27 @@ module top_module(
 	end
 endmodule
 
-// Using for-loop
-/* 
-        module top_module(
-            input clk,
-            input load,
-            input [511:0] data,
-            output [511:0] q ); 
+    // Using for-loop
+    /* 
+    module top_module(
+        input clk,
+        input load,
+        input [511:0] data,
+        output [511:0] q ); 
             
-            always @(posedge clk)begin
-                if (load)
-                    q <= data;
-                else begin
+        always @(posedge clk)begin
+            if (load)
+                q <= data;
+            else begin
                     for (integer i=0; i<512; i=i+1 )begin
-                        if (i == 0)
-                            q[i] <= 0 ^ q[i+1];
-                        else if (i == 511)
-                            q[i] <= 0 ^ q[i-1];
-                        else
-                            q[i] <= q[i-1] ^ q[i+1];
-                        end
-                end
-            end
-        endmodule
- */
+                    if (i == 0)
+                        q[i] <= 0 ^ q[i+1];
+                    else if (i == 511)
+                        q[i] <= 0 ^ q[i-1];
+                    else
+                        q[i] <= q[i-1] ^ q[i+1];
+                    end
+             end
+        end
+    endmodule
+    */
