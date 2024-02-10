622d1d970eb2a1114692aaed34aa5cd39563a0a0
Raveem KG <gkraveem@gmail.com>
2024-02-10 12:24:03 +0530

    Latches & Flip-flops solved

--- 3-Circuits-Sequential Logic/10-Latches and Flip-Flops/092-MuxandDff-2014q4a.v
+++ 3-Circuits-Sequential Logic/10-Latches and Flip-Flops/092-MuxandDff-2014q4a.v
@@ -1,12 +1,11 @@
 module top_module (
-	input clk,
-	input L,
-	input r_in,
-	input q_in,
-	output reg Q);
+    input clk,
+    input w, R, E, L,
+    output Q
+);
     always @(posedge clk) begin
         Q <= L ? R : ( E ? w : Q) ;
     end
 endmodule
 
-// Write a Verilog module (containing one flip-flop and multiplexer) named top_module for this submodule.
+// Write a Verilog module (containing one flip-flop and 2 multiplexer) named top_module for this submodule.
