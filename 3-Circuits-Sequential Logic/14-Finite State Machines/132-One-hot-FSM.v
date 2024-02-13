00ddf5424f198b562ec0614bdcb705bff705669b
Raveem KG <gkraveem@gmail.com>
2024-02-13 13:49:39 +0530

    Finite State Machines solutions added

--- 3-Circuits-Sequential Logic/14-Finite State Machines/132-One-hot-FSM.v
+++ 3-Circuits-Sequential Logic/14-Finite State Machines/132-One-hot-FSM.v
@@ -4,9 +4,9 @@ module top_module(
     output [9:0] next_state,
     output out1,
     output out2);
+    
     //-------------Internal Constants-----------------
     parameter S0=0, S1=1, S2=2, S3=3, S4=4, S5=5, S6=6, S7=7, S8=8, S9=9;
-    //-------------Internal Variables-----------------
 
     // State transition logic - Combinational Logic
     assign next_state[S0] = ~in & |( state & 10'b1110011111 );
