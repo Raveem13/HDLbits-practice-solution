ce693269aa46117838fc1eea7b5408402746c84d
Raveem KG <gkraveem@gmail.com>
2024-02-18 10:19:34 +0530

    Building Larger Circuits solved

--- 3-Circuits-Sequential Logic/15-Building Larger Circuits/157-Thecomplete-timer-r2015-fancytimer.v
+++ 3-Circuits-Sequential Logic/15-Building Larger Circuits/157-Thecomplete-timer-r2015-fancytimer.v
@@ -6,15 +6,32 @@ module top_module (
     output counting,
     output done,
     input ack );
-    
-        //-------------Internal Constants-----------------
+
+    //-------------Internal Constants-----------------
     parameter S=0, S1=1, S11=2, S110=3, B0=4, B1=5, B2=6, B3=7, Count=8, Wait=9 ;  // States 
 
     //-------------Internal Variables-----------------
     reg [3:0] state, next_state;   
-    wire shift_ena, count_ena ;
-    wire done_counting;
-    reg [9:0] inner_count;
+    reg shift_ena, count_ena, sc1_ena ; 	// Intermediate enable signals used
+    reg done_counting, sc1_c0 ;				
+    reg [9:0] q;							// Inner 1k-counter output
+    
+    // 1k counter instantiation
+    counter1k c1k (
+        .clk(clk),
+        .rst(count_ena), 
+        //.ena_in(count_ena), 
+        //.ena_out(sc1_ena),
+        .qout(q)
+    );
+    
+    and ( sc1_ena, q[0], q[1], q[2], q[5], q[6], q[7], q[8], q[9] );	// Count 1000 cycles then enable signal to shift+down counter  
+    nor ( sc1_c0, count[0], count[1], count[2], count[3] );
+    and ( done_counting, sc1_ena, sc1_c0);  							// set flag done_counting if outer count = 0 & inner count = 999
+    
+    // shift register & down counter instantiation
+    shiftcount sc1 ( clk, shift_ena, sc1_ena, data, count );   
+    
     
     // State transition logic - Combinational Logic - The Complete FSM
     always @(*) begin
@@ -26,10 +43,13 @@ module top_module (
             B0 : next_state = B1;
             B1 : next_state = B2;
             B2 : next_state = B3;
-            B3 : next_state = Count;
+            B3 : begin
+                //count_ena = 1;
+                next_state = Count;
+            end
             Count : next_state = done_counting ? Wait : Count;
             Wait : next_state = ack ? S : Wait;
-            //default : next_state = 3'bxx;
+            default : next_state = 4'bx;
         endcase
     end
 
@@ -42,45 +62,51 @@ module top_module (
             state <= next_state;
     end
 
-    count1k inner_counter (
-    	.clk(clk),
-    	.reset(reset),
-    	.q(inner_count)
-	);
-
-    
-    // 4-bit shift register and down counter
-    always @(posedge clk) begin
-        if(shift_ena)
-            count <= ( count<<1 ) | data;
-        if (count_ena)	begin
-            //if(count == 0) 
-                //done_counting = 1;
-            if (inner_count == 999) begin
-            	count <= count - 1; // Increment the outer counter when the inner counter reaches its maximum value
-        	end
-            
-        end
-    end
-    
     // Output logic - Combinational output logic - The Complete FSM
-    assign shift_ena = (state == B0) | (state == B1) | (state == B2) | (state == B3);
-    assign count_ena = ~ shift_ena ;
+    assign count_ena =  (state == B3) ;  // Control signals to start counter -> reset inner counter
+    assign shift_ena = (state == B0) || (state == B1) || (state == B2) || (state == B3);  // Control signals to shift data
+    
     assign counting = ( state == Count );
-    assign done = ( state == Wait );
+    assign done = (state == Wait);
 
 endmodule
 
-module count1k (
+// 0-999 => 1k clock cycles counter module
+module counter1k (
     input clk,
-    input reset,
-    output [9:0] q);
+    input rst,
+    // input ena_in,
+    // output ena_out,
+    output [9:0] qout );
     
-    always @(posedge clk)begin
-        if(reset || q == 999)
-            q <= 0;
-        else
-            q <= q + 1;
+    always @(posedge clk) begin
+        if(rst)
+            qout <= 0;
+        else if ( qout == 999 ) begin
+            // ena_out <= 1;
+            qout <= 0;
+        end
+        else begin
+            qout <= qout + 1;
+            // ena_out <= 0;
+        end
     end
 
+endmodule
+
+// 4-bit shift register and down counter module
+module shiftcount (
+    input clk,
+    input s_ena,
+    input c_ena,
+    input data,
+    output [3:0] count );
+    
+    always @(posedge clk) begin
+        if(s_ena)
+            count <= ( count<<1 ) | data;
+        if (c_ena)	
+            count <= count - 1;
+    end    
+    
 endmodule
