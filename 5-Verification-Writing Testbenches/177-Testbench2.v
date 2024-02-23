e7d3fc07c37fed677b7e580fc65c1167e9a212c9
Raveem KG <gkraveem@gmail.com>
2024-02-23 17:33:54 +0530

    Writing Testbenches solved

--- 5-Verification-Writing Testbenches/177-Testbench2.v
+++ 5-Verification-Writing Testbenches/177-Testbench2.v
@@ -23,3 +23,31 @@ module top_module();
     end
 
 endmodule
+
+    /*
+
+    module top_module();
+
+        reg clk, i, o;
+        reg [2:0] s;
+        
+        q7 dut1( clk, i, s,  o);
+        
+        initial
+            clk = 0;
+        
+        always #5 clk = ~clk; //generate Clock with period = 10ns
+        
+        // generate input patterns here
+        initial begin
+            i = 0; s = 2; #10;
+            s = 6; #10;
+            i = 1; s = 2; #10;
+            i = 0; s = 7; #10;
+            i = 1; s = 0; #30;
+            i = 0; #10;
+        end
+
+    endmodule
+
+    */
